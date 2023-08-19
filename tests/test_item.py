8d02531cfd7ab92e4a2803fc97b1bb5d1fc5dddb
Evgeniy <Darkorbitint1@yandex.ru>
2023-08-19 21:10:24 +0300

    1111

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+item = Item("Смартфон", 10000, 20)
+def test_init():
+    assert item.name == "Смартфон"
+    assert item.price == 10000
+    assert item.quantity == 20
+    assert item.calculate_total_price() == 200000
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000
+
+
+
